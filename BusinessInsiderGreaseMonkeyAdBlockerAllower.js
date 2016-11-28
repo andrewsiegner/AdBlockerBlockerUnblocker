dc932dd047907938cc313384ce3c48fb39504080
Andrew Siegner <andrewsiegner@gmail.com>
2016-11-28 18:50:52 -0500

    Updated Version

--- BusinessInsiderGreaseMonkeyAdBlockerAllower.js
+++ BusinessInsiderGreaseMonkeyAdBlockerAllower.js
@@ -5,28 +5,28 @@
 // @grant       none
 // ==/UserScript==
 var myInterval;
-var count = 0;
+
 
 function removeAdBlockerBlocker() {
-  
-  
-
+//Grab the parent element which in this case is body
 var parent = document.getElementsByTagName("body")[0];
-
+  
+//Set overflow to scroll on body, its set to hidden when the ad-blocker is detected
 parent.style.overflow = "scroll";
-
+  
+//grab the child elements that block the page when an ad-blocker is detected
 var child = document.getElementsByClassName("tp-modal")[0];
-  
 var backDrop = document.getElementsByClassName("tp-backdrop")[0];
-  if (child != null) {
-      parent.removeChild(child);
-      parent.removeChild(backDrop);
-      count++;
+  
+  //if those blocking elements have been appended (as soon as the blocking elements get appended remove them and break check/delete loop
+  if (child != null && backDrop != null) {
+       parent.removeChild(child);
+       parent.removeChild(backDrop);
+       clearInterval(myInterval);
   }
   
-  if (count > 0) {
-    clearInterval(myInterval);
-  }
 }
 
-//myInterval = setInterval(removeAdBlockerBlocker, 2000);
+//Set this function on a timed loop, running once every 2 seconds until it succeeds at removing the blocking elements. 
+//The blocking elements are not added immediately, so the function is called once every 2 seconds until they do and can be removed.
+myInterval = setInterval(removeAdBlockerBlocker, 2000);
